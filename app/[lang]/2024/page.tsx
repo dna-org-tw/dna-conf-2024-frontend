f4d29c4d40d14dea460ecf867b27caf92a2c5d12
loischen68 <a8283626@gmail.com>
2024-03-14 22:56:52 +0800

    feat: add agenda component

--- app/[lang]/2024/page.tsx
+++ app/[lang]/2024/page.tsx
@@ -1,20 +1,25 @@
 import Link from "next/link";
-import {useServerTranslation} from "@/i18n";
-import {LangParams} from "@/types/common";
+import { useServerTranslation } from "@/i18n";
+import { LangParams } from "@/types/common";
 import Speaker from "../../components/speaker";
 import HeaderTitleWithLine from "@/app/components/HeaderTitleWithLine";
 import Banner from "@/app/components/Banner";
+import Agenda from "@/app/components/Agenda";
 
-export default async function Home({params: {lang}}: { params: LangParams }) {
-  const {t} = await useServerTranslation(lang);
+export default async function Home({
+  params: { lang },
+}: {
+  params: LangParams;
+}) {
+  const { t } = await useServerTranslation(lang);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="flex justify-center gap-4 fixed top-0 left-0 bg-black/30 text-white p-8 m-4">
+      <div className="flex justify-center gap-4 fixed top-0 left-0 bg-black/30 text-white p-8 m-4 z-10">
         <Link href="/en-US/2024">en-US</Link>
         <Link href="/zh-TW/2024">zh-TW</Link>
       </div>
-      <Banner lang={lang}/>
+      <Banner lang={lang} />
       <section className="w-full py-20 px-24">
         <HeaderTitleWithLine
           title={t("KEYNOTE SPEAKER")}
@@ -27,9 +32,7 @@ export default async function Home({params: {lang}}: { params: LangParams }) {
           imgSrc="/images/osera-ryo.png"
         />
       </section>
-      <section className="w-full bg-[#D3D3D3] py-20 px-24">
-        <HeaderTitleWithLine title={t("AGENDA")} lineColor="red"/>
-      </section>
+      <Agenda lang={lang} />
       <section className="w-full py-20 px-24">
         <HeaderTitleWithLine
           title={t("TRANSPORTATION INFORMATION")}
@@ -37,7 +40,7 @@ export default async function Home({params: {lang}}: { params: LangParams }) {
         />
       </section>
       <section className="w-full py-20 px-24">
-        <HeaderTitleWithLine title={t("CALL FOR SPONSOR")} lineColor="yellow"/>
+        <HeaderTitleWithLine title={t("CALL FOR SPONSOR")} lineColor="yellow" />
       </section>
     </main>
   );
