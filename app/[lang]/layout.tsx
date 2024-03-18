9dbbc673b2907df362e7f7398fbf58649eab6387
Yuanlin Lin <im.yuanlinlin@gmail.com>
2024-03-18 20:46:51 +0800

    fix: favicon and meta image

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -13,19 +13,19 @@ export function generateMetadata( { params }: { params: LangParams } ): Metadata
         metadataBase: new URL('https://conf.dna.org.tw'),
         title: "TDNA Taiwan Digital Nomad Association",
         description: "Facilitating the career development and networking of digital nomads by integrating resources from various companies. We make the digital nomad life less lonely!",
-        icons: [{ url: "/favicon.ico", type: "image/x-icon" }],
+        icons: [{ url: "/images/favicon.ico", type: "image/x-icon" }],
         openGraph: {
           locale: "en_US",
           type: "website",
           title: "TDNA Taiwan Digital Nomad Association",
           description: "Facilitating the career development and networking of digital nomads by integrating resources from various companies. We make the digital nomad life less lonely!",
-          images: [{ url: "/meta.jpg" }],
+          images: [{ url: "/images/meta.jpg" }],
           url: "http://conf.dna.org.tw/en-US/2024",
         },
         twitter: {
           title: "TDNA Taiwan Digital Nomad Association",
           description: "Facilitating the career development and networking of digital nomads by integrating resources from various companies. We make the digital nomad life less lonely!",
-          images: [{ url: "/meta.jpg" }],
+          images: [{ url: "/images/meta.jpg" }],
           card: "summary_large_image",
         }
       };
@@ -34,19 +34,19 @@ export function generateMetadata( { params }: { params: LangParams } ): Metadata
         metadataBase: new URL('https://conf.dna.org.tw'),
         title: "TDNA 台灣數位遊牧者協會",
         description: "透過整合各個公司的資源，協助數位遊牧者的職涯發展與交流。 我們讓數位遊牧生活不再孤單！",
-        icons: [{ url: "/favicon.ico", type: "image/x-icon" }],
+        icons: [{ url: "/images/favicon.ico", type: "image/x-icon" }],
         openGraph: {
           locale: "zh-TW",
           type: "website",
           title: "TDNA 台灣數位遊牧者協會",
           description: "透過整合各個公司的資源，協助數位遊牧者的職涯發展與交流。 我們讓數位遊牧生活不再孤單！",
-          images: [{ url: "/meta.jpg" }],
+          images: [{ url: "/images/meta.jpg" }],
           url: "http://conf.dna.org.tw/zh-TW/2024",
         },
         twitter: {
           title: "TDNA 台灣數位遊牧者協會",
           description: "透過整合各個公司的資源，協助數位遊牧者的職涯發展與交流。 我們讓數位遊牧生活不再孤單！",
-          images: [{ url: "/meta.jpg" }],
+          images: [{ url: "/images/meta.jpg" }],
           card: "summary_large_image",
         }
       };
