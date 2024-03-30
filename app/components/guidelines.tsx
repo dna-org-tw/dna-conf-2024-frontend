cfd41723fe48b5b58d10b34afe3fd715f58d176d
yui <>
2024-03-31 00:16:51 +0800

    modify: delete collapsable components of pc

--- app/components/guidelines.tsx
+++ app/components/guidelines.tsx
@@ -1,7 +1,8 @@
-'use client';
+"use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
+import clsx from "clsx";
 
 interface buttonText {
   expand: string;
@@ -12,57 +13,89 @@ interface IProps {
   open?: boolean;
   title: string;
   children: React.ReactNode;
-  buttonText: buttonText
+  buttonText: buttonText;
 }
 
-
-const Guidelines: React.FC<IProps> = ({ open, children, title, buttonText }) => {
-  
+const Guidelines: React.FC<IProps> = ({
+  open,
+  children,
+  title,
+  buttonText,
+}) => {
   const [isOpen, setIsOpen] = useState(open);
+  const [showButton, setShowButton] = useState(false);
 
   const collapsibleHandler = () => {
     setIsOpen((prev) => !prev);
   };
 
+  useEffect(() => {
+    setShowButton(isSMScreen());
+  }, [isOpen]);
+
+  function isSMScreen() {
+    if (typeof window !== "undefined") {
+      return window.innerWidth < 640;
+    }
+    return false;
+  }
+
   return (
     <div className="container mx-auto mx-2 mx-0 lg:whitespace-nowrap lg:pt-10 lg:pb-20 lg:px-8">
-      <h6 className="mt-4 lg:text-[16px] text-black flex-2 font-bold text-center tracking-widest mb-2">
-        {title}
-      </h6>
-      <div className="lg:whitespace-normal lg:pl-4 xl:pl-10">
-        {!isOpen && <ol className="list-decimal tracking-widest mt-4 m-16 mb-10 mr-8 pl-2 lg:ml-6 xl:ml-20">
+      <div className="relative lg:whitespace-normal lg:pl-4 xl:pl-10">
+        <div
+          className={clsx("absolute left-0 top-0 w-full h-full", {
+            "bg-[linear-gradient(to_bottom,transparent,white)]": !isOpen,
+          })}
+        ></div>
+        <h6 className="mt-4 lg:text-[16px] text-black flex-2 font-bold text-center tracking-widest mb-2">
+          {title}
+        </h6>
+        <ol
+          className={clsx(
+            "list-decimal tracking-widest mt-4 ml-8 mb-10 mr-8 pl-6 lg:ml-6 xl:ml-20 overflow-hidden",
+            { "h-[140px]": !isOpen, "h-auto": isOpen }
+          )}
+        >
           {React.Children.map(children, (notice, index) => (
-            <li key={index} className="mb-2 md:mb-4">
-              {typeof notice === 'string' && notice.includes("us@dna.org.tw") ? (
-                <p dangerouslySetInnerHTML={{ __html: notice.replace("us@dna.org.tw", '<u>us@dna.org.tw</u>') }} />
+            <li key={index} className="mb-4 md:mb-4">
+              {typeof notice === "string" &&
+              notice.includes("us@dna.org.tw") ? (
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: notice.replace(
+                      "us@dna.org.tw",
+                      "<u>us@dna.org.tw</u>"
+                    ),
+                  }}
+                />
               ) : (
                 notice
               )}
             </li>
           ))}
-        </ol>}
+        </ol>
       </div>
-      <div className="relative flex flex-col items-center justify-center">
-        <button type="button" className="mx-auto mt-4 mb-2 lg:mt-3" onClick={collapsibleHandler}>
-          {!isOpen ? (
-            <Image
-            src="/images/button-up.png"
-            alt="Collapsible button up"
-            width={18}
-            height={15}
-            />
+      <div className="flex flex-col items-center justify-center mt-4">
+        {showButton && (
+          <button type="button" onClick={collapsibleHandler}>
+            {!isOpen ? (
+              <Image
+                src="/images/button-angle-down.png"
+                alt="Collapsible button down"
+                width={25}
+                height={15}
+              />
             ) : (
               <Image
-              src="/images/button-down.png"
-              alt="Collapsible button down"
-              width={18}
-              height={15}
+                src="/images/button-angle-up.png"
+                alt="Collapsible button up"
+                width={25}
+                height={15}
               />
-              )}
-        </button>
-          <div className="lg:p-2 text-[#7D7D77] text-sm tracking-widest">
-            {isOpen ? buttonText.expand: buttonText.collapse}
-          </div>
+            )}
+          </button>
+        )}
       </div>
     </div>
   );
