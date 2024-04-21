f6cef6dc75bb5cae047f07cdb823626f4de9a619
loischen68 <a8283626@gmail.com>
2024-04-22 04:43:48 +0800

    feat: carousel speaker slide

---
+++ app/components/Carousel/CarouselSlide.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { ReactElement, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+
+export default function Slide({
+  children,
+  slideIndex,
+  updateCurrentSlide,
+}: {
+  children: ReactElement[];
+  slideIndex: number;
+  updateCurrentSlide: (slideIndex: number) => void;
+}) {
+  const [ref, inView] = useInView({
+    threshold: 1,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      updateCurrentSlide(slideIndex);
+    }
+  }, [inView, slideIndex, updateCurrentSlide]);
+
+  return (
+    <div
+      id={`slide-${slideIndex}`}
+      key={slideIndex}
+      ref={ref}
+      className={`grid grid-cols-1 w-full snap-center	 sm:grid-cols-2 md:grid-cols-3`}
+    >
+      {children}
+    </div>
+  );
+}
